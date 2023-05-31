d9aeb632e69d7fe7227f5e3631167ec5c24c02ca
Nandolim3 <nandolim3@gmail.com>
2023-05-30 21:26:45 -0300

    Initial commit

---
+++ app/src/main/java/com/example/barbeariasalvatore/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.barbeariasalvatore
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.barbeariasalvatore.databinding.ActivityMainBinding
+import com.example.barbeariasalvatore.view.Home
+import com.google.android.material.snackbar.Snackbar
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.btLogin.setOnClickListener{
+
+            val nome = binding.editNome.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            when{
+                nome.isEmpty() -> {
+                    mensagem(it,"Coloque o seu nome!")
+                }senha.isEmpty() -> {
+                    mensagem(it, "Preencha a senha!")
+                }senha.length <=5 -> {
+                    mensagem(it, "A senha precisa ter pel menos 6 caractere!")
+                }else -> {
+                    navegarPraHome(nome)
+                }
+            }
+        }
+    }
+
+    private fun mensagem(view: View, mensagem: String){
+        val snackbar = Snackbar.make(view,mensagem,Snackbar.LENGTH_SHORT)
+        snackbar.setBackgroundTint(Color.parseColor("#FF0000"))
+        snackbar.setTextColor(Color.parseColor("#FFFFFF"))
+        snackbar.show()
+    }
+
+    private fun navegarPraHome(nome: String){
+        val intent = Intent(this,Home::class.java)
+        intent.putExtra("nome",nome)
+        startActivity(intent)
+    }
+}
